4ae0c5dbc01418d2b412d6456b2affc1d1afe749
Chillbok <45466997+Chillbok@users.noreply.github.com>
2025-05-07 05:24:45 +0900

    영상 18 실습 완료

--- Assets/Scripts/Movement2D.cs
+++ Assets/Scripts/Movement2D.cs
@@ -2,12 +2,21 @@ using UnityEngine;
 
 public class Movement2D : MonoBehaviour
 {
-    [SerializeField]
-    private float moveSpeed = 0.0f;
-    [SerializeField]
-    private Vector3 moveDirection = Vector3.zero;
+    [SerializeField] private float moveSpeed = 0.0f;
+    [SerializeField] private Vector3 moveDirection = Vector3.zero;
+    private float baseMoveSpeed;
 
-    public float MoveSpeed => moveSpeed; //moveSpeed 변수의 프로퍼티(Property) (Get 가능)
+    //moveSpeed 변수의 프로퍼티(Property) (Set, Get 가능)
+    public float MoveSpeed
+    {
+        set => moveSpeed = Mathf.Max(0, value);
+        get => moveSpeed;
+    }
+
+    private void Awake()
+    {
+        baseMoveSpeed = moveSpeed;
+    }
 
     private void Update()
     {
@@ -17,6 +26,11 @@ public class Movement2D : MonoBehaviour
     public void MoveTo(Vector3 direction)
     {
         moveDirection = direction;
+    }
+
+    public void ResetMoveSpeed()
+    {
+        moveSpeed = baseMoveSpeed;
     }
 }
 
